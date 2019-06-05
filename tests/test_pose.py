16c4a8e3a282dd5fccfc2be14e2c3c71328246de
Philipp Tempel <mail@philipptempel.me>
2019-06-05 07:49:41 +0200

    Update testss for pose to work with `scipy.spatial.transform`

--- tests/test_pose.py
+++ tests/test_pose.py
@@ -1,81 +1,65 @@
 from unittest import TestCase
 
 import numpy as np
-import quaternion
+from scipy.spatial.transform import Rotation
 
 from cdpyr.motion import Pose
 
 
 class TestPose(TestCase):
+    rotation: Rotation
+    position: np.ndarray
+    time: float
+
+    def setUp(self):
+        self.rotation = Rotation.random(num=1)[0]
+        self.position = np.random.rand(3)
+        self.time = np.random.rand(1)[0]
+
     def test_position_is_initialized_correctly(self):
-        p = np.random.rand(3)
-        pose = Pose(position=p)
+        pose = Pose(position=self.position, time=self.time)
 
-        np.testing.assert_array_almost_equal(p, pose.position)
-        np.testing.assert_almost_equal(np.eye(3), pose.rotation_matrix)
-        np.testing.assert_almost_equal(np.eye(3).reshape(9),
-                                       pose.orientation)
-        np.testing.assert_almost_equal(0, pose.time)
+        np.testing.assert_allclose(pose.position, self.position)
+        np.testing.assert_allclose(pose.dcm, np.eye(3))
+        np.testing.assert_allclose(pose.time, self.time)
 
-    def test_orientation_is_initialized_correctly_by_rotation_matrix(self):
-        q = quaternion.from_rotation_vector(np.random.random_sample(3))
-        r: np.ndarray = quaternion.as_rotation_matrix(q)
-        o = r.reshape(9)
-        pose = Pose(rotation_matrix=r)
+    def test_orientation_is_initialized_correctly_by_dcm(self):
+        pose = Pose(dcm=self.rotation.as_dcm(), time=self.time,
+                    position=self.position)
 
-        np.testing.assert_array_almost_equal(r, pose.rotation_matrix)
-        np.testing.assert_array_almost_equal(o, pose.orientation)
-        np.testing.assert_array_almost_equal(
-            np.asarray(q, np.quaternion).view((np.float64, 4)),
-            np.asarray(pose.quaternion, np.quaternion).view((np.float64, 4)))
-        np.testing.assert_almost_equal(np.zeros(3), pose.position)
-        np.testing.assert_almost_equal(0, pose.time)
+        np.testing.assert_allclose(pose.dcm, self.rotation.as_dcm())
+        np.testing.assert_allclose(pose.position, self.position)
+        np.testing.assert_allclose(pose.time, self.time)
 
-    def test_orientation_is_initialized_correctly_by_orientation(self):
-        q = quaternion.from_rotation_vector(np.random.random_sample(3))
-        r: np.ndarray = quaternion.as_rotation_matrix(q)
-        o = r.reshape(9)
-        pose = Pose(orientation=o)
+    def test_orientation_is_initialized_correctly_by_quaternion(self):
+        pose = Pose(quaternion=self.rotation.as_quat(), time=self.time,
+                    position=self.position)
 
-        np.testing.assert_array_almost_equal(r, pose.rotation_matrix)
-        np.testing.assert_array_almost_equal(o, pose.orientation)
-        np.testing.assert_array_almost_equal(
-            np.asarray(q, np.quaternion).view((np.float64, 4)),
-            np.asarray(pose.quaternion, np.quaternion).view((np.float64, 4)))
-        np.testing.assert_almost_equal(np.zeros(3), pose.position)
-        np.testing.assert_almost_equal(0, pose.time)
+        np.testing.assert_allclose(pose.quaternion, self.rotation.as_quat())
+        np.testing.assert_allclose(pose.dcm, self.rotation.as_dcm())
+        np.testing.assert_allclose(pose.position, self.position)
+        np.testing.assert_allclose(pose.time, self.time)
 
-    def test_orientation_is_initialized_correctly_by_quaternin(self):
-        q: np.ndarray = quaternion.from_rotation_vector(
-            np.random.random_sample(3))
-        r = quaternion.as_rotation_matrix(q)
-        pose = Pose(quaternion=q)
+    def test_orientation_is_initialized_correctly_by_rotation(self):
+        pose = Pose(orientation=self.rotation, time=self.time,
+                    position=self.position)
 
-        np.testing.assert_array_almost_equal(
-            np.asarray(q, np.quaternion).view((np.float64, 4)),
-            np.asarray(pose.quaternion, np.quaternion).view((np.float64, 4)))
-        np.testing.assert_array_almost_equal(r, pose.rotation_matrix)
-        np.testing.assert_array_almost_equal(r.reshape(9), pose.orientation)
-        np.testing.assert_almost_equal(np.zeros(3), pose.position)
-        np.testing.assert_almost_equal(0, pose.time)
+        np.testing.assert_allclose(pose.quaternion, self.rotation.as_quat())
+        np.testing.assert_allclose(pose.dcm, self.rotation.as_dcm())
+        np.testing.assert_allclose(pose.position, self.position)
+        np.testing.assert_allclose(pose.time, self.time)
 
     def test_pose_to_str(self):
-        t = np.random.random_sample()
-        p = np.random.rand(3)
-        o: np.ndarray = quaternion.as_rotation_matrix(
-            quaternion.from_rotation_vector(np.random.random_sample(3)))
+        pose = Pose(position=self.position, dcm=self.rotation.as_dcm(), time=self.time)
 
-        pose = Pose(position=p, rotation_matrix=o, time=t)
-
-        expected = np.array2string(np.hstack([t, p, o.reshape(9)]),
+        expected = np.array2string(np.hstack([self.time, self.position,
+                                              self.rotation.as_dcm().reshape(9)]),
                                    separator=',',
                                    formatter={'float_kind': lambda x: "%.2f" %
                                                                       x})[1:-1]
-        np.testing.assert_array_almost_equal(p, pose.position)
-        np.testing.assert_array_almost_equal(o, pose.rotation_matrix)
-        np.testing.assert_array_almost_equal(o.reshape(9),
-                                             pose.orientation)
-        np.testing.assert_almost_equal(t, pose.time)
+        np.testing.assert_allclose(pose.position, self.position)
+        np.testing.assert_allclose(pose.dcm, self.rotation.as_dcm())
+        np.testing.assert_allclose(pose.time, self.time)
         self.assertEqual(expected, str(pose))
 
     def test_compare_two_poses_lt(self):
