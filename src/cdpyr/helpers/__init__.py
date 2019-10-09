46cfbcbe414ad14459983d16f954d0f87e718620
Philipp Tempel <git@philipptempel.me>
2019-10-09 13:32:28 +0200

    Add helper to format time into human readable format

---
+++ src/cdpyr/helpers/__init__.py
@@ -0,0 +1,25 @@
+from typing import AnyStr, Optional
+
+units = {"nsec": 1e-9, "usec": 1e-6, "msec": 1e-3, "sec": 1.0}
+
+
+def format_time(dt, time_unit: Optional[AnyStr] = None, precision: int = None):
+    unit = time_unit
+
+    if unit is not None:
+        scale = units[unit]
+    else:
+        scales = [(scale, unit) for unit, scale in units.items()]
+        scales.sort(reverse=True)
+        for scale, unit in scales:
+            if dt >= scale:
+                break
+
+    return "%.*g %s" % (precision if precision is not None else 3,
+                        dt / scale,
+                        unit)
+
+
+__all__ = [
+    'format_time'
+]
