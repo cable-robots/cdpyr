0a38c169bbd8c1f4e859e8dee34b356ff9084ccb
Philipp Tempel <git@philipptempel.me>
2019-10-09 11:35:02 +0200

    Add more validators for numeric and vectorial data

--- src/cdpyr/validator/__init__.py
+++ src/cdpyr/validator/__init__.py
@@ -1,7 +1,11 @@
-from cdpyr.validator.validator import nonnegative, nonzero, positive
+from cdpyr.validator.validator import negative, nonzero, positive, shape, dimensions, nonnegative, nonpositive
 
 __all__ = [
+    'dimensions',
+    'negative',
     'nonnegative',
+    'nonpositive',
     'nonzero',
-    'positive'
+    'positive',
+    'shape',
 ]
--- src/cdpyr/validator/validator.py
+++ src/cdpyr/validator/validator.py
@@ -1,13 +1,85 @@
-def nonzero(value):
-    if value == 0:
-        raise ValueError('value must be nonzero')
+from typing import Sequence, Union
+
+import numpy as np_
+
+from cdpyr.typing import Num, Vector, Matrix
 
 
-def nonnegative(value):
-    if value < 0:
-        raise ValueError('value must be nonnegative')
+def nonzero(value: Union[Num, Vector, Sequence[Num]], name: str = None):
+    value = np_.asarray(value)
+    if np_.any(value == 0):
+        raise ValueError(
+            'Expected `{}` to be {}nonzero.'.format(
+                name if name is not None else 'value',
+                'all ' if value.size > 1 else ''
+            )
+        )
 
 
-def positive(value):
-    if value > 0:
-        raise ValueError('value must be positive')
+def negative(value: Union[Num, Vector, Sequence[Num]], name: str = None):
+    value = np_.asarray(value)
+    if np_.any(value >= 0):
+        raise ValueError(
+            'Expected `{}` to be {}negative.'.format(
+                name if name is not None else 'value',
+                'all ' if value.size > 1 else ''
+            )
+        )
+
+
+def nonnegative(value: Union[Num, Vector, Sequence[Num]], name: str = None):
+    value = np_.asarray(value)
+    if np_.any(value < 0):
+        raise ValueError(
+            'Expected `{}` to be {}nonnegative.'.format(
+                name if name is not None else 'value',
+                'all ' if value.size > 1 else ''
+            )
+        )
+
+
+def positive(value: Union[Num, Vector, Sequence[Num]], name: str = None):
+    value = np_.asarray(value)
+    if np_.any(value <= 0):
+        raise ValueError(
+            'Expected `{}` to be {}positive.'.format(
+                name if name is not None else 'value',
+                'all ' if value.size > 1 else ''
+            )
+        )
+
+
+def nonpositive(value: Union[Num, Vector, Sequence[Num]], name: str = None):
+    value = np_.asarray(value)
+    if np_.any(value > 0):
+        raise ValueError(
+            'Expected `{}` to be {}positive.'.format(
+                name if name is not None else 'value',
+                'all ' if value.size > 1 else ''
+            )
+        )
+
+
+def dimensions(value: Union[Num, Vector, Matrix, Sequence[Num]], expected: int, name: str = None):
+    value = np_.asarray(value)
+    if value.ndim != expected:
+        raise ValueError(
+            'Expected `{}` to have {} dimension{}, got {} instead.'.format(
+                name if name is not None else 'value',
+                expected,
+                's' if expected > 1 else '',
+                value.ndim
+            )
+        )
+
+
+def shape(value: Union[Num, Vector, Matrix, Sequence[Num]], expected: Union[int, tuple], name: str = None):
+    value = np_.asarray(value)
+    if value.shape != expected:
+        raise ValueError(
+            'Expected `{}` to have shape {}, got {} instead.'.format(
+                name if name is not None else 'value',
+                expected,
+                value.shape
+            )
+        )
