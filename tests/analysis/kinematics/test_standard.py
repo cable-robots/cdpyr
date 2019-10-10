0b55f7b32d5ef0ddee5dfe01c4eebefb0049186d
Philipp Tempel <git@philipptempel.me>
2019-10-10 16:48:59 +0200

    Use robot fixtures in standard inverse kinematics test

--- tests/analysis/kinematics/test_standard.py
+++ tests/analysis/kinematics/test_standard.py
@@ -2,101 +2,99 @@ from typing import Sequence
 
 import numpy as np
 import pytest
-import cdpyr
+
 from cdpyr.typing import Matrix, Num, Vector
-from sample import robots
 
 
 class StandardKinematicsTestSuite(object):
 
-    def test_motionpattern_1t(self, rand_pose_1t, ik_standard):
-        robot = robots.robot_1t()
-
+    def test_motionpattern_1t(self, robot_1t, rand_pose_1t, ik_standard):
         length: Vector
         unit_directions: Matrix
-        length, unit_directions = ik_standard.backward(robot, rand_pose_1t)
+        length, unit_directions = ik_standard.backward(robot_1t, rand_pose_1t)
 
         # TODO implement the test for `forward`, too
 
         assert length.ndim == 1
-        assert length.shape == (robot.num_kinematic_chains,)
+        assert length.shape == (robot_1t.num_kinematic_chains,)
         assert (length >= 0).all()
-        assert unit_directions.shape == (3, robot.num_kinematic_chains)
-        assert unit_directions[1:,:] == pytest.approx(np.zeros((2, robot.num_kinematic_chains)))
-        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(np.ones(robot.num_kinematic_chains, ))
+        assert unit_directions.shape == (3, robot_1t.num_kinematic_chains)
+        assert unit_directions[1:, :] == pytest.approx(
+            np.zeros((2, robot_1t.num_kinematic_chains)))
+        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(
+            np.ones(robot_1t.num_kinematic_chains, ))
 
-    def test_motionpattern_2t(self, rand_pose_2t, ik_standard):
-        robot = robots.robot_2t()
-
+    def test_motionpattern_2t(self, robot_2t, rand_pose_2t, ik_standard):
         length: Vector
         unit_directions: Matrix
-        length, unit_directions = ik_standard.backward(robot, rand_pose_2t)
+        length, unit_directions = ik_standard.backward(robot_2t, rand_pose_2t)
 
         # TODO implement the test for `forward`, too
 
         assert length.ndim == 1
-        assert length.shape == (robot.num_kinematic_chains,)
+        assert length.shape == (robot_2t.num_kinematic_chains,)
         assert (length >= 0).all()
-        assert unit_directions.shape == (3, robot.num_kinematic_chains)
-        assert unit_directions[2:,:] == pytest.approx(np.zeros((1, robot.num_kinematic_chains)))
-        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(np.ones(robot.num_kinematic_chains, ))
+        assert unit_directions.shape == (3, robot_2t.num_kinematic_chains)
+        assert unit_directions[2:, :] == pytest.approx(
+            np.zeros((1, robot_2t.num_kinematic_chains)))
+        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(
+            np.ones(robot_2t.num_kinematic_chains, ))
 
-    def test_motionpattern_3t(self, rand_pose_3t, ik_standard):
-        robot = robots.robot_3t()
-
+    def test_motionpattern_3t(self, robot_3t, rand_pose_3t, ik_standard):
         length: Sequence[Num]
         unit_directions: Matrix
-        length, unit_directions = ik_standard.backward(robot, rand_pose_3t)
+        length, unit_directions = ik_standard.backward(robot_3t, rand_pose_3t)
 
         # TODO implement the test for `forward`, too
 
         assert length.ndim == 1
-        assert length.shape == (robot.num_kinematic_chains,)
+        assert length.shape == (robot_3t.num_kinematic_chains,)
         assert (length >= 0).all()
-        assert unit_directions.shape == (3, robot.num_kinematic_chains)
-        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(np.ones(robot.num_kinematic_chains, ))
+        assert unit_directions.shape == (3, robot_3t.num_kinematic_chains)
+        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(
+            np.ones(robot_3t.num_kinematic_chains, ))
 
-    def test_motionpattern_1r2t(self, rand_pose_1r2t, ik_standard):
-        robot = robots.robot_1r2t()
-
+    def test_motionpattern_1r2t(self, robot_1r2t, rand_pose_1r2t, ik_standard):
         length: Vector
         unit_directions: Matrix
-        length, unit_directions = ik_standard.backward(robot, rand_pose_1r2t)
+        length, unit_directions = ik_standard.backward(robot_1r2t,
+                                                       rand_pose_1r2t)
 
         # TODO implement the test for `forward`, too
 
         assert length.ndim == 1
-        assert length.shape == (robot.num_kinematic_chains,)
+        assert length.shape == (robot_1r2t.num_kinematic_chains,)
         assert (length >= 0).all()
-        assert unit_directions.shape == (3, robot.num_kinematic_chains)
-        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(np.ones(robot.num_kinematic_chains, ))
+        assert unit_directions.shape == (3, robot_1r2t.num_kinematic_chains)
+        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(
+            np.ones(robot_1r2t.num_kinematic_chains, ))
 
-    def test_motionpattern_2r3t(self, rand_pose_2r3t, ik_standard):
-        robot = robots.robot_2r3t()
-
+    def test_motionpattern_2r3t(self, robot_2r3t, rand_pose_2r3t, ik_standard):
         length: Vector
         unit_directions: Matrix
-        length, unit_directions = ik_standard.backward(robot, rand_pose_2r3t)
+        length, unit_directions = ik_standard.backward(robot_2r3t,
+                                                       rand_pose_2r3t)
 
         # TODO implement the test for `forward`, too
 
         assert length.ndim == 1
-        assert length.shape == (robot.num_kinematic_chains,)
+        assert length.shape == (robot_2r3t.num_kinematic_chains,)
         assert (length >= 0).all()
-        assert unit_directions.shape == (3, robot.num_kinematic_chains)
-        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(np.ones(robot.num_kinematic_chains, ))
+        assert unit_directions.shape == (3, robot_2r3t.num_kinematic_chains)
+        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(
+            np.ones(robot_2r3t.num_kinematic_chains, ))
 
-    def test_motionpattern_3r3t(self, rand_pose_3r3t, ik_standard):
-        robot = robots.robot_3r3t()
-
+    def test_motionpattern_3r3t(self, robot_3r3t, rand_pose_3r3t, ik_standard):
         length: Vector
         unit_directions: Matrix
-        length, unit_directions = ik_standard.backward(robot, rand_pose_3r3t)
+        length, unit_directions = ik_standard.backward(robot_3r3t,
+                                                       rand_pose_3r3t)
 
         # TODO implement the test for `forward`, too
 
         assert length.ndim == 1
-        assert length.shape == (robot.num_kinematic_chains,)
+        assert length.shape == (robot_3r3t.num_kinematic_chains,)
         assert (length >= 0).all()
-        assert unit_directions.shape == (3, robot.num_kinematic_chains)
-        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(np.ones(robot.num_kinematic_chains, ))
+        assert unit_directions.shape == (3, robot_3r3t.num_kinematic_chains)
+        assert np.linalg.norm(unit_directions, axis=0) == pytest.approx(
+            np.ones(robot_3r3t.num_kinematic_chains, ))
