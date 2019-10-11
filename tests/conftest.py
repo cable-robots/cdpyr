7604d79b33eedfea4bf7c54f10b9927ca53c7580
Philipp Tempel <git@philipptempel.me>
2019-10-11 18:02:07 +0200

    Fix wrong robot fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -80,7 +80,7 @@ def robot_2t():
                         position=[0., 0., 0.],
                     ),
                     cdpyr.robot.PlatformAnchor(
-                        position=[0., -0., 0.],
+                        position=[0., 0., 0.],
                     ),
                 ]
             ),
@@ -127,18 +127,12 @@ def robot_3t():
                 cdpyr.robot.FrameAnchor(
                     position=[0., -1., 1.],
                 ),
-                cdpyr.robot.FrameAnchor(
-                    position=[0., 0., 0.],
-                ),
             ]
         ),
         platforms=[
             cdpyr.robot.Platform(
                 motionpattern=cdpyr.motion.Motionpattern.MP_3T,
                 anchors=[
-                    cdpyr.robot.PlatformAnchor(
-                        position=[0., 0., 0.],
-                    ),
                     cdpyr.robot.PlatformAnchor(
                         position=[0., 0., 0.],
                     ),
@@ -170,18 +164,11 @@ def robot_3t():
                 diameter=6 / 1000,
                 color="red"
             ),
-            cdpyr.robot.Cable(
-                name="Dyneema",
-                material="Liros D-Pro SK78",
-                diameter=6 / 1000,
-                color="red"
-            ),
         ],
         kinematic_chains=[
             {'frame_anchor': 0, 'platform': 0, 'platform_anchor': 0, 'cable': 0},
             {'frame_anchor': 1, 'platform': 0, 'platform_anchor': 1, 'cable': 1},
             {'frame_anchor': 2, 'platform': 0, 'platform_anchor': 2, 'cable': 2},
-            {'frame_anchor': 3, 'platform': 0, 'platform_anchor': 3, 'cable': 3},
         ],
     )
 
@@ -498,14 +485,14 @@ def robot_3r3t():
             ),
         ],
         kinematic_chains=[
-            {'frame_anchor': 0, 'platform': 0, 'platform_anchor': 4, 'cable': 0},
-            {'frame_anchor': 1, 'platform': 0, 'platform_anchor': 5, 'cable': 1},
-            {'frame_anchor': 2, 'platform': 0, 'platform_anchor': 6, 'cable': 2},
-            {'frame_anchor': 3, 'platform': 0, 'platform_anchor': 7, 'cable': 3},
-            {'frame_anchor': 4, 'platform': 0, 'platform_anchor': 0, 'cable': 4},
-            {'frame_anchor': 5, 'platform': 0, 'platform_anchor': 1, 'cable': 5},
-            {'frame_anchor': 6, 'platform': 0, 'platform_anchor': 2, 'cable': 6},
-            {'frame_anchor': 7, 'platform': 0, 'platform_anchor': 3, 'cable': 7},
+            {'frame_anchor': 0, 'platform': 0, 'platform_anchor': 0, 'cable': 0},
+            {'frame_anchor': 1, 'platform': 0, 'platform_anchor': 1, 'cable': 1},
+            {'frame_anchor': 2, 'platform': 0, 'platform_anchor': 2, 'cable': 2},
+            {'frame_anchor': 3, 'platform': 0, 'platform_anchor': 3, 'cable': 3},
+            {'frame_anchor': 4, 'platform': 0, 'platform_anchor': 4, 'cable': 4},
+            {'frame_anchor': 5, 'platform': 0, 'platform_anchor': 5, 'cable': 5},
+            {'frame_anchor': 6, 'platform': 0, 'platform_anchor': 6, 'cable': 6},
+            {'frame_anchor': 7, 'platform': 0, 'platform_anchor': 7, 'cable': 7},
         ],
     )
 
