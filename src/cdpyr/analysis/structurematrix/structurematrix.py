07bbce2ef9002396a3b199b8b5018bba9d5a5990
Philipp Tempel <git@philipptempel.me>
2019-10-11 17:58:34 +0200

    Require `Robot` object in structure matrix calculation
    Will later on be used to get the gravitational wrench automatically

--- src/cdpyr/analysis/structurematrix/structurematrix.py
+++ src/cdpyr/analysis/structurematrix/structurematrix.py
@@ -1,5 +1,6 @@
-from typing import Sequence, Union
+from typing import Dict, Sequence, Union
 
+import numpy as np_
 from magic_repr import make_repr
 
 from cdpyr.analysis.structurematrix.algorithm import (
@@ -23,20 +24,23 @@ class StructureMatrix(object):
         _motionpattern.Motionpattern.MP_1T.name:   _mp1t.StructureMatrix1T(),
         _motionpattern.Motionpattern.MP_2T.name:   _mp2t.StructureMatrix2T(),
         _motionpattern.Motionpattern.MP_3T.name:   _mp3t.StructureMatrix3T(),
-        _motionpattern.Motionpattern.MP_1R2T.name: _mp1r2t.StructureMatrix1R2T(),
-        _motionpattern.Motionpattern.MP_2R3T.name: _mp2r3t.StructureMatrix2R3T(),
+        _motionpattern.Motionpattern.MP_1R2T.name:
+                                                   _mp1r2t.StructureMatrix1R2T(),
+        _motionpattern.Motionpattern.MP_2R3T.name:
+                                                   _mp2r3t.StructureMatrix2R3T(),
         _motionpattern.Motionpattern.MP_3R3T.name: _mp3r3t.StructureMatrix3R3T()
     }
 
-    def __call__(self,
-                 robot: '_robot.Robot',
-                 uis: Union[Matrix, Sequence[Matrix]],
-                 pose: Union['_pose.Pose', Sequence['_pose.Pose']],
-                 ):
+    @classmethod
+    def calculate(cls,
+                  robot: '_robot.Robot',
+                  uis: Union[Matrix, Sequence[Matrix]],
+                  pose: Union['_pose.Pose', Sequence['_pose.Pose']] = None,
+                  ):
         # and make sure we are dealing with a sequence of poses
         pose = pose if isinstance(pose, Sequence) else [pose]
 
-        # Make a multi
+        # make uis a list of matrices
         uis = uis if isinstance(uis, Sequence) else [uis]
 
         # ensure we have just as many uis and poses as platforms
@@ -55,7 +59,7 @@ class StructureMatrix(object):
         # loop over each platform and calculate its structure matrix
         for idx, platform in enumerate(robot.platforms):
             # calculate the platforms structure matrix
-            structurematrices.append(self._MAPPING[
+            structurematrices.append(cls._MAPPING[
                                          platform.motionpattern.name
                                      ].calculate(platform, uis[idx], pose[idx])
                                      )
@@ -65,6 +69,13 @@ class StructureMatrix(object):
         return structurematrices[0] \
             if robot.num_platforms == 1 \
             else structurematrices
+
+    def __call__(self,
+                 robot: '_robot.Robot',
+                 uis: Union[Matrix, Sequence[Matrix]],
+                 pose: Union['_pose.Pose', Sequence['_pose.Pose']] = None,
+                 ):
+        return self.calculate(robot, uis, pose)
 
     def _resolve_structure_matrix(self, platform, pose, ui):
         pass
