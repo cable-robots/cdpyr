c4b4e07ee85219a7271279168cae0762248efbc9
Philipp Tempel <git@philipptempel.me>
2019-10-15 20:30:31 +0200

    Add pose generators for `steps` and `interval` type of lists

--- src/cdpyr/motion/__init__.py
+++ src/cdpyr/motion/__init__.py
@@ -1,8 +1,10 @@
 from cdpyr.motion.pattern import Motionpattern
 from cdpyr.motion.pose import Pose, PoseList
+from cdpyr.motion.pose import generator
 
 __all__ = [
     'Pose',
     'PoseList',
     'Motionpattern',
+    'generator',
 ]
--- src/cdpyr/motion/pose/__init__.py
+++ src/cdpyr/motion/pose/__init__.py
@@ -1,7 +1,9 @@
 from cdpyr.motion.pose.pose import Pose
-from cdpyr.motion.pose.poselist import PoseList
+from cdpyr.motion.pose.list import List as PoseList
+from cdpyr.motion.pose import generator
 
 __all__ = [
     'Pose',
     'PoseList',
+    'generator'
 ]
