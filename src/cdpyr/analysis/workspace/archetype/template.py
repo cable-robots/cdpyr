9b4d5967e069395be141af18aa28f3a648158307
Philipp Tempel <git@philipptempel.me>
2019-10-23 18:01:00 +0200

    Add template modules for archetype, criterion, and method of workspace package

---
+++ src/cdpyr/analysis/workspace/archetype/template.py
@@ -0,0 +1,9 @@
+from cdpyr.analysis.workspace.archetype import archetype as _archetype
+from cdpyr.typing import Vector
+
+comparator = all
+
+
+def poses(archetype: '_archetype.Archetype', coordinate: Vector):
+    # and return the generator
+    raise NotImplementedError
---
+++ src/cdpyr/analysis/workspace/method/template.py
@@ -0,0 +1,22 @@
+from typing import Any, AnyStr, Dict, Sequence, Tuple
+
+from cdpyr.analysis.workspace import workspace as _calculator
+from cdpyr.analysis.workspace.archetype import archetype as _archetype
+from cdpyr.analysis.workspace.criterion import criterion as _criterion
+from cdpyr.analysis.workspace.method import method as _method
+from cdpyr.robot import robot as _robot
+
+
+def evaluate(self: '_method.Method',
+             robot: '_robot.Robot',
+             calculator: '_calculator.Calculator',
+             archetype: '_archetype.Archetype',
+             criteria: Sequence[Tuple[
+                 '_criterion.Criterion',
+                 Dict[AnyStr, Any]
+             ]]):
+    raise NotImplementedError
+
+
+__vars__ = [
+]
