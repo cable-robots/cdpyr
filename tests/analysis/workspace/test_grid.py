d6b47842e2b222a2403de01b7f4d3d8404e821c0
Philipp Tempel <git@philipptempel.me>
2019-10-23 18:02:07 +0200

    Add test cases for `Grid` and `Hull` workspace methods

---
+++ tests/analysis/workspace/test_grid.py
@@ -0,0 +1,404 @@
+from matplotlib import use
+
+use('MacOSX')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import numpy as np
+
+import cdpyr
+
+
+def plot_workspace(dims, coordinates):
+    # filter for the inside and outside coordinates
+    inside = np.asarray([coordinate[0] for coordinate in coordinates if all(coordinate[1].values())])
+    outside = np.asarray([coordinate[0] for coordinate in coordinates if not all(coordinate[1].values())])
+
+    fig: plt.Figure = plt.figure()
+    # three dimensional case?
+    if dims == 3:
+        ax: Axes3D = fig.add_subplot(111, projection='3d')
+        if inside.size:
+            ax.plot(inside[:, 0], inside[:, 1], inside[:, 2],
+                    marker='o', linestyle='None', color=[0.0, 1.0, 0.0])
+        if outside.size:
+            ax.plot(outside[:, 0], outside[:, 1], outside[:, 2],
+                    marker='o', markersize=2,
+                    linestyle='None', color=[1.0, 0.0, 0.0])
+    # two-dimensional case
+    elif dims == 2:
+        ax: plt.Axes = fig.add_subplot(111)
+        if inside.size:
+            ax.plot(inside[:, 0], inside[:, 1],
+                    marker='o', linestyle='None', color=[0.0, 1.0, 0.0])
+        if outside.size:
+            ax.plot(outside[:, 0], outside[:, 1],
+                    marker='o', markersize=2,
+                    linestyle='None', color=[1.0, 0.0, 0.0])
+    # one-dimensional case
+    else:
+        ax: plt.Axes = fig.add_subplot(111)
+        if inside.size:
+            ax.plot(inside[:, 0], np.zeros_like(inside[:, 0]),
+                    marker='o', linestyle='None', color=[0.0, 1.0, 0.0])
+        if outside.size:
+            ax.plot(outside[:, 0], np.zeros_like(outside[:, 0]),
+                    marker='o', markersize=2,
+                    linestyle='None', color=[1.0, 0.0, 0.0])
+
+    fig.show()
+
+
+class GridWorkspaceTestSuite(object):
+
+    def test_1t_ik_standard_translation_cable_length(self,
+                                                     robot_1t:
+                                                     'cdpyr.robot.Robot',
+                                                     ik_standard:
+                                                     'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            # (
+            #     cdpyr.analysis.workspace.Criterion.CABLE_LENGTH
+            # )
+            (
+                cdpyr.analysis.workspace.Criterion.CABLE_LENGTH,
+                {
+                    'limits': [0.5, 1.5],
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate of '-1.0' to
+        # a maximum coordinate of '1.0'
+        method.min = -1.0
+        method.max = 1.0
+        # along the grid, we want 51 steps which equals to 52 coordinates in
+        # total including the corners
+        method.step = 49
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_1t)
+
+        plot_workspace(1, workspace)
+
+        assert False
+
+    def test_1t_ik_standard_translation_singularities(self,
+                                                      robot_1t:
+                                                      'cdpyr.robot.Robot',
+                                                      ik_standard:
+                                                      'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.SINGULARITIES
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate of '-1.0' to
+        # a maximum coordinate of '1.0'
+        method.min = -1.0
+        method.max = 1.0
+        # along the grid, we want 51 steps which equals to 52 coordinates in
+        # total including the corners
+        method.step = 49
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_1t)
+
+        plot_workspace(1, workspace)
+
+        assert False
+
+    def test_1t_ik_standard_translation_wrench_closure(self,
+                                                        robot_1t:
+                                                        'cdpyr.robot.Robot',
+                                                        ik_standard:
+                                                        'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.WRENCH_CLOSURE,
+                {
+                    'wrench': -1,
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate of '-1.0' to
+        # a maximum coordinate of '1.0'
+        method.min = -1.0
+        method.max = 1.0
+        # along the grid, we want 51 steps which equals to 52 coordinates in
+        # total including the corners
+        method.step = 49
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_1t)
+
+        plot_workspace(1, workspace)
+
+        assert False
+
+    def test_1t_ik_standard_translation_wrench_feasible(self,
+                                                        robot_1t:
+                                                        'cdpyr.robot.Robot',
+                                                        ik_standard:
+                                                        'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.WRENCH_FEASIBLE,
+                {
+                    'wrench': -1,
+                    'force_min': 1,
+                    'force_max': 10,
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate of '-1.0' to
+        # a maximum coordinate of '1.0'
+        method.min = -1.0
+        method.max = 1.0
+        # along the grid, we want 51 steps which equals to 52 coordinates in
+        # total including the corners
+        method.step = 49
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_1t)
+
+        plot_workspace(1, workspace)
+
+        assert False
+
+    def test_3r3t_ik_standard_translation_cable_length(self,
+                                                       robot_3r3t:
+                                                       'cdpyr.robot.Robot',
+                                                       ik_standard:
+                                                       'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            # (
+            #     cdpyr.analysis.workspace.Criterion.CABLE_LENGTH
+            # )
+            (
+                cdpyr.analysis.workspace.Criterion.CABLE_LENGTH,
+                {
+                    'limits': [0.866025404, 2.598076211],
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate in the bottom
+        # left rear edge of the unit cube to a maximum coordinate in the
+        # right front corner of the unit cube
+        method.min = [-1.0, -1.0, -1.0]
+        method.max = [1.0, 1.0, 1.0]
+        # along the grid, we want different steps: 4 along `x`, 4 along `y`,
+        # and 9 along `z`
+        method.step = [4, 4, 9]
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_3r3t)
+
+        plot_workspace(3, workspace)
+
+        assert False
+
+    def test_3r3t_ik_standard_translation_wrench_closure(self,
+                                                          robot_3r3t:
+                                                          'cdpyr.robot.Robot',
+                                                          ik_standard:
+                                                          'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.WRENCH_CLOSURE,
+                {
+                    'wrench': [0.0, 0.0, -9.81, 0.0, 0.0, 0.0],
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate in the bottom
+        # left rear edge of the unit cube to a maximum coordinate in the
+        # right front corner of the unit cube
+        method.min = [-1.0, -1.0, -1.0]
+        method.max = [1.0, 1.0, 1.0]
+        # along the grid, we want different steps: 4 along `x`, 4 along `y`,
+        # and 9 along `z`
+        method.step = [4, 4, 9]
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_3r3t)
+
+        plot_workspace(3, workspace)
+
+        assert False
+
+    def test_3r3t_ik_standard_translation_wrench_feasible(self,
+                                                          robot_3r3t:
+                                                          'cdpyr.robot.Robot',
+                                                          ik_standard:
+                                                          'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.WRENCH_FEASIBLE,
+                {
+                    'wrench': [0.0, 0.0, -9.81, 0.0, 0.0, 0.0],
+                    'force_min': 1,
+                    'force_max': 10,
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate in the bottom
+        # left rear edge of the unit cube to a maximum coordinate in the
+        # right front corner of the unit cube
+        method.min = [-1.0, -1.0, -1.0]
+        method.max = [1.0, 1.0, 1.0]
+        # along the grid, we want different steps: 4 along `x`, 4 along `y`,
+        # and 9 along `z`
+        method.step = [4, 4, 9]
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_3r3t)
+
+        plot_workspace(3, workspace)
+
+        assert False
+
+    def test_3r3t_ik_standard_translation_singularities(self,
+                                                        robot_3r3t:
+                                                        'cdpyr.robot.Robot',
+                                                        ik_standard:
+                                                        'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.SINGULARITIES
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.GRID
+        # configure our grid to range from a minimum coordinate in the bottom
+        # left rear edge of the unit cube to a maximum coordinate in the
+        # right front corner of the unit cube
+        method.min = [-1.0, -1.0, -1.0]
+        method.max = [1.0, 1.0, 1.0]
+        # along the grid, we want different steps: 4 along `x`, 4 along `y`,
+        # and 9 along `z`
+        method.step = [4, 4, 9]
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace = workspace_calculator.evaluate(robot_3r3t)
+
+        plot_workspace(3, workspace)
+
+        assert False
---
+++ tests/analysis/workspace/test_hull.py
@@ -0,0 +1,183 @@
+import numpy as np
+import vtk
+
+import cdpyr
+
+
+def draw_workspace(vertices: np.ndarray, faces: np.ndarray):
+    colors = vtk.vtkNamedColors()
+
+    # create polyhedron (cube)
+    # The point Ids are: [0, 1, 2, 3, 4, 5, 6, 7]
+
+    points = vtk.vtkPoints()
+    for vertex in vertices:
+        points.InsertNextPoint(*vertex)
+
+    # These are the point ids corresponding to each face.
+    faceId = vtk.vtkIdList()
+    faceId.InsertNextId(faces.shape[0])  # Six faces make up the cell.
+    for face in faces:
+        faceId.InsertNextId(len(face))  # The number of points in the face.
+        [faceId.InsertNextId(i) for i in face]
+
+    ugrid = vtk.vtkUnstructuredGrid()
+    ugrid.SetPoints(points)
+    ugrid.InsertNextCell(vtk.VTK_POLYHEDRON, faceId)
+
+    # Create a mapper and actor
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(ugrid)
+
+    writer = vtk.vtkXMLUnstructuredGridWriter()
+    writer.SetInputData(ugrid)
+    writer.SetFileName('polyhedron.vtu')
+    writer.SetDataModeToAscii()
+    writer.Update()
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetColor(colors.GetColor3d('Silver'))
+
+    # Visualize
+    renderer = vtk.vtkRenderer()
+    window = vtk.vtkRenderWindow()
+    window.SetSize(1600, 1000)
+    window.SetWindowName('Polyhedron')
+    window.AddRenderer(renderer)
+    window_interactor = vtk.vtkRenderWindowInteractor()
+    window_interactor.SetRenderWindow(window)
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d('Salmon'))
+    renderer.ResetCamera()
+    renderer.GetActiveCamera().Azimuth(30)
+    renderer.GetActiveCamera().Elevation(30)
+    window.Render()
+    window_interactor.Start()
+
+
+class GridWorkspaceTestSuite(object):
+
+    def test_3r3t_ik_standard_translation_cable_length(self,
+                                                       robot_3r3t:
+                                                       'cdpyr.robot.Robot',
+                                                       ik_standard:
+                                                       'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.CABLE_LENGTH,
+                {
+                    'limits': [np.sqrt(0.5 ** 2 + 0.5 ** 2 + 0.5 ** 2),
+                               np.sqrt(1.5 ** 2 + 1.5 ** 2 + 1.5 ** 2)],
+                }
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.HULL
+        # place center of hull at the world origin
+        method.center = [0.0, 0.0, 0.0]
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace, faces = workspace_calculator.evaluate(robot_3r3t)
+
+        # get corners of workspace
+        corners = np.asarray([p[0] for p in workspace])
+
+        draw_workspace(corners, faces)
+
+        assert False
+
+    def test_3r3t_ik_standard_translation_singularities(self,
+                                                        robot_3r3t:
+                                                        'cdpyr.robot.Robot',
+                                                        ik_standard:
+                                                        'cdpyr.analysis.kinematics.Calculator'):
+        # workspace archetype we want to calculate
+        archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+        archetype.dcm = np.eye(3)
+
+        # criteria and their parameters we want to evaluate
+        criteria = [
+            (
+                cdpyr.analysis.workspace.Criterion.SINGULARITIES
+            )
+        ]
+
+        # method we want to use to calculate the workspace
+        method = cdpyr.analysis.workspace.Method.HULL
+        # place center of hull at the world origin
+        method.center = [0.0, 0.0, 0.0]
+
+        # a workspace calculator object
+        workspace_calculator = cdpyr.analysis.workspace.Calculator(
+            archetype,
+            method,
+            criteria,
+            kinematics=ik_standard)
+
+        # evaluate the workspace over the grid
+        workspace, faces = workspace_calculator.evaluate(robot_3r3t)
+
+        # get corners of workspace
+        corners = np.asarray([p[0] for p in workspace])
+
+        draw_workspace(corners, faces)
+
+        assert False
+
+    # def test_3r3t_ik_standard_translation_wrench_feasible(self,
+    #                                                       robot_3r3t:
+    #                                                       'cdpyr.robot.Robot',
+    #                                                       ik_standard:
+    #                                                       'cdpyr.analysis.kinematics.Calculator'):
+    #     # workspace archetype we want to calculate
+    #     archetype = cdpyr.analysis.workspace.Archetype.TRANSLATION
+    #     archetype.dcm = np.eye(3)
+    #
+    #     # criteria and their parameters we want to evaluate
+    #     criteria = [
+    #         (
+    #             cdpyr.analysis.workspace.Criterion.WRENCH_FEASIBLE,
+    #             {
+    #                 'wrench':    [0.0, 0.0, -9.81, 0.0, 0.0, 0.0],
+    #                 'force_min': 1,
+    #                 'force_max': 10,
+    #             }
+    #         )
+    #     ]
+    #
+    #     # method we want to use to calculate the workspace
+    #     method = cdpyr.analysis.workspace.Method.HULL
+    #     # place center of hull at the world origin
+    #     method.center = [0.0, 0.0, 0.0]
+    #
+    #     # a workspace calculator object
+    #     workspace_calculator = cdpyr.analysis.workspace.Calculator(
+    #         archetype,
+    #         method,
+    #         criteria,
+    #         kinematics=ik_standard)
+    #
+    #     # evaluate the workspace over the grid
+    #     workspace, faces = workspace_calculator.evaluate(robot_3r3t)
+    #
+    #     # get corners of workspace
+    #     corners = np.asarray([p[0] for p in workspace])
+    #
+    #     draw_workspace(corners, faces)
+    #
+    #     assert False
