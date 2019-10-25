e0f24218a0639942b73eb3e7b9a9bc86610cf450
Philipp Tempel <git@philipptempel.me>
2019-10-25 12:17:47 +0200

    Add a sample 3R3T robot with 2 platforms

--- sample/robots.py
+++ sample/robots.py
@@ -647,3 +647,319 @@ def robot_3r3t():
             },
         ],
     )
+
+
+def robot_3r3t_2platform():
+    return cdpyr.robot.Robot(
+        name="Sample 3R3T robot with 2 platforms",
+        frame=cdpyr.robot.Frame(
+            anchors=[
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, 1.0, 1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, 1.0, 1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, -1.0, 1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, -1.0, 1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, 1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, 1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, -1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, -1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, 1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, 1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, -1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, -1.0, 0.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, 1.0, -1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, 1.0, -1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[1.0, -1.0, -1.0]
+                ),
+                cdpyr.robot.FrameAnchor(
+                    position=[-1.0, -1.0, -1.0]
+                ),
+            ]
+        ),
+        platforms=[
+            cdpyr.robot.Platform(
+                motionpattern=cdpyr.motion.Motionpattern.MP_3R3T,
+                anchors=[
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, 0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, 0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, -0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, -0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, 0.1, -0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, 0.1, -0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, -0.1, -0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, -0.1, -0.1]
+                    ),
+                ],
+                name="upper platform",
+            ),
+            cdpyr.robot.Platform(
+                motionpattern=cdpyr.motion.Motionpattern.MP_3R3T,
+                anchors=[
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, 0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, 0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, -0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, -0.1, 0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, 0.1, -0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, 0.1, -0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[0.1, -0.1, -0.1]
+                    ),
+                    cdpyr.robot.PlatformAnchor(
+                        position=[-0.1, -0.1, -0.1]
+                    ),
+                ],
+                name="lower platform",
+            )
+        ],
+        cables=[
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+            cdpyr.robot.Cable(
+                name="Dyneema",
+                material="Liros D-Pro SK78",
+                diameter=6 / 1000,
+                color="red"
+            ),
+        ],
+        kinematic_chains=[
+            {
+                'frame_anchor':    0,
+                'platform':        0,
+                'platform_anchor': 0,
+                'cable':           0
+            },
+            {
+                'frame_anchor':    1,
+                'platform':        0,
+                'platform_anchor': 1,
+                'cable':           1
+            },
+            {
+                'frame_anchor':    2,
+                'platform':        0,
+                'platform_anchor': 2,
+                'cable':           2
+            },
+            {
+                'frame_anchor':    3,
+                'platform':        0,
+                'platform_anchor': 3,
+                'cable':           3
+            },
+            {
+                'frame_anchor':    4,
+                'platform':        0,
+                'platform_anchor': 4,
+                'cable':           4
+            },
+            {
+                'frame_anchor':    5,
+                'platform':        0,
+                'platform_anchor': 5,
+                'cable':           5
+            },
+            {
+                'frame_anchor':    6,
+                'platform':        0,
+                'platform_anchor': 6,
+                'cable':           6
+            },
+            {
+                'frame_anchor':    7,
+                'platform':        0,
+                'platform_anchor': 7,
+                'cable':           7
+            },
+            {
+                'frame_anchor':    8,
+                'platform':        1,
+                'platform_anchor': 0,
+                'cable':           0
+            },
+            {
+                'frame_anchor':    9,
+                'platform':        1,
+                'platform_anchor': 1,
+                'cable':           1
+            },
+            {
+                'frame_anchor':    10,
+                'platform':        1,
+                'platform_anchor': 2,
+                'cable':           2
+            },
+            {
+                'frame_anchor':    11,
+                'platform':        1,
+                'platform_anchor': 3,
+                'cable':           3
+            },
+            {
+                'frame_anchor':    12,
+                'platform':        1,
+                'platform_anchor': 4,
+                'cable':           4
+            },
+            {
+                'frame_anchor':    13,
+                'platform':        1,
+                'platform_anchor': 5,
+                'cable':           5
+            },
+            {
+                'frame_anchor':    14,
+                'platform':        1,
+                'platform_anchor': 6,
+                'cable':           6
+            },
+            {
+                'frame_anchor':    15,
+                'platform':        1,
+                'platform_anchor': 7,
+                'cable':           7
+            },
+        ],
+    )
