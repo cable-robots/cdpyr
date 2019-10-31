40b86aca7c38fee5c2c47b7a9bd01fdcc9a66f18
Philipp Tempel <git@philipptempel.me>
2019-10-31 14:30:39 +0100

    Correct use of new kinematics calculators

--- tests/conftest.py
+++ tests/conftest.py
@@ -746,4 +746,4 @@ def rand_pose_3d(rand_pos_3t, rand_rot_3r):
 
 @pytest.fixture
 def ik_standard():
-    return cdpyr.analysis.kinematics.Calculator.STANDARD
+    return cdpyr.analysis.kinematics.STANDARD()
