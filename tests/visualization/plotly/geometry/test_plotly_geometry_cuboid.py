cee4ef77c1ef93462248747b3e0aaf3a7001b1b0
Philipp Tempel <git@philipptempel.me>
2019-11-21 15:26:38 +0100

    Add test cases for plotly rendering geometry primitives

---
+++ tests/visualization/plotly/geometry/test_plotly_geometry_cuboid.py
@@ -0,0 +1,30 @@
+import pytest
+
+from cdpyr import visualization
+from cdpyr.motion import Pose
+from cdpyr.robot import Robot
+from cdpyr.geometry import Cuboid
+
+__author__ = "Philipp Tempel"
+__email__ = "p.tempel@tudelft.nl"
+
+
+class PlotGeometryCuboidTestSuite(object):
+
+    def test_plot_spatial_tube(self,
+                             tmpdir):
+        # create a cuboid
+        cuboid = Cuboid(1.0, 0.5, 0.25)
+
+        # and visualize
+        visualizer = visualization.plotly.SPATIAL()
+        visualizer.render(cuboid)
+        visualizer.draw()
+        visualizer.show()
+        # visualizer.figure.savefig(
+        #     f'{tmpdir / sys._getframe().f_code.co_name}_empty')
+        visualizer.close()
+
+
+if __name__ == "__main__":
+    pytest.main()
