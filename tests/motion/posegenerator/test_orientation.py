ec5d8760629f2f4476d7cb4134b32fb926af6fd1
Philipp Tempel <git@philipptempel.me>
2019-11-26 18:59:19 +0100

    Add tests cases for new pose generator

---
+++ tests/motion/posegenerator/test_orientation.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pytest
+
+from cdpyr.motion.pose import generator_new as generator
+
+__author__ = "Philipp Tempel"
+__email__ = "p.tempel@tudelft.nl"
+
+
+class PoseGeneratorOrientationTestSuite(object):
+
+    def tests_linear_default_step(self):
+        poses = generator.orientation(0.0, 0.5 * np.pi, 'x')
+
+    def tests_linear_custom_step(self):
+        poses = generator.orientation(0.0, 0.5 * np.pi, 'x', step=25)
+        with pytest.raises(Exception):
+            poses = generator.orientation(0.0, 0.5 * np.pi, 'x', step=[25, 25])
+
+    def tests_planar_default_step(self):
+        poses = generator.orientation([0.0, 0.0],
+                                      [0.5 * np.pi, 1.0 * np.pi],
+                                      'xy')
+
+    def tests_planar_custom_step(self):
+        poses = generator.orientation(
+            [0.0, 0.0],
+            [0.5 * np.pi, 1.0 * np.pi],
+            'xy',
+            step=25)
+        poses = generator.orientation(
+            [0.0, 0.0],
+            [0.5 * np.pi, 1.0 * np.pi],
+            'xy',
+            step=[25, 25])
+        with pytest.raises(Exception):
+            poses = generator.orientation(
+                [0.0, 0.0],
+                [0.5 * np.pi, 1.0 * np.pi],
+                'xy',
+                step=[25, 25, 25])
+
+    def tests_spatial_default_step(self):
+        poses = generator.orientation(
+            [0.0, 0.0, 0.0],
+            [0.5 * np.pi, 1.0 * np.pi, 1.5 * np.pi],
+            'xyz',
+            step=25)
+        poses = generator.orientation(
+            [0.0, 0.0, 0.0],
+            [0.5 * np.pi, 1.0 * np.pi, 1.5 * np.pi],
+            'xyz',
+            step=[25, 25, 25])
+        with pytest.raises(Exception):
+            poses = generator.orientation(
+                [0.0, 0.0, 0.0],
+                [0.5 * np.pi, 1.0 * np.pi, 1.5 * np.pi],
+                'xyz',
+                step=[25, 25])
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ tests/motion/posegenerator/test_translation.py
@@ -0,0 +1,54 @@
+import pytest
+
+from cdpyr.motion.pose import generator_new as generator
+
+__author__ = "Philipp Tempel"
+__email__ = "p.tempel@tudelft.nl"
+
+
+class PoseGeneratorTranslationTestSuite(object):
+
+    def tests_linear_default_step(self):
+        poses = generator.translation(0.0, 1.0)
+
+    def tests_linear_custom_step(self):
+        poses = generator.translation(0.0, 1.0, step=25)
+        with pytest.raises(Exception):
+            poses = generator.translation(0.0, 1.0, step=[25, 25])
+
+    def tests_planar_default_step(self):
+        poses = generator.translation([0.0, 0.0], [1.0, 2.0])
+
+    def tests_planar_custom_step(self):
+        poses = generator.translation(
+            [0.0, 0.0],
+            [1.0, 2.0],
+            step=25)
+        poses = generator.translation(
+            [0.0, 0.0],
+            [1.0, 2.0],
+            step=[25, 25])
+        with pytest.raises(Exception):
+            poses = generator.translation(
+                [0.0, 0.0],
+                [1.0, 2.0],
+                step=[25, 25, 25])
+
+    def tests_spatial_default_step(self):
+        poses = generator.translation(
+            [0.0, 0.0, 0.0],
+            [1.0, 2.0, 3.0],
+            step=25)
+        poses = generator.translation(
+            [0.0, 0.0, 0.0],
+            [1.0, 2.0, 3.0],
+            step=[25, 25, 25])
+        with pytest.raises(Exception):
+            poses = generator.translation(
+                [0.0, 0.0, 0.0],
+                [1.0, 2.0, 3.0],
+                step=[25, 25])
+
+
+if __name__ == "__main__":
+    pytest.main()
