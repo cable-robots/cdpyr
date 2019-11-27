5835440d2c82361a49479bbff571aeb722b25e3e
Philipp Tempel <git@philipptempel.me>
2019-11-27 16:39:28 +0100

    Rewrite test cases for workspace to include more fixtures and split by motion pattern

---
+++ tests/analysis/workspace/grid/test_grid_2t.py
@@ -0,0 +1,158 @@
+# import sys
+from typing import Union
+
+# import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+from cdpyr.analysis import (
+    force_distribution,
+    workspace
+)
+from cdpyr.analysis.kinematics.algorithm import Algorithm as Kinematics
+from cdpyr.analysis.workspace.archetype.archetype import Archetype
+from cdpyr.kinematics.transformation import Angular
+from cdpyr.robot import Robot
+from cdpyr.typing import (
+    Num,
+    Vector
+)
+
+
+# from mpl_toolkits.mplot3d import Axes3D
+
+
+class GridWorkspace2TtTestSuite(object):
+
+    @pytest.mark.parametrize(
+        ['archetype', 'lower_bound', 'upper_bound', 'steps'],
+        (
+            (
+                workspace.archetype.Translation(dcm),
+                [-1.0, -1.0],
+                [1.0, 1.0],
+                29
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_2t_cable_length(self,
+                             robot_2t: Robot,
+                             ik_standard: Kinematics,
+                             archetype: Archetype,
+                             lower_bound: Union[Num, Vector],
+                             upper_bound: Union[Num, Vector],
+                             steps: Union[Num, Vector]):
+        # create the criterion
+        criterion = workspace.criterion.CableLength(ik_standard, [0.5, 1.5])
+
+        # create the grid calculator object
+        calculator = workspace.GridCalculator(ik_standard,
+                                              archetype,
+                                              criterion,
+                                              lower_bound,
+                                              upper_bound,
+                                              steps)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_2t)
+
+    @pytest.mark.parametrize(
+        ['archetype', 'lower_bound', 'upper_bound', 'steps'],
+        (
+            (
+                workspace.archetype.Translation(dcm),
+                [-1.0, -1.0],
+                [1.0, 1.0],
+                29
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_2t_singularities(self,
+                              robot_2t: Robot,
+                              ik_standard: Kinematics,
+                              archetype: Archetype,
+                              lower_bound: Union[Num, Vector],
+                              upper_bound: Union[Num, Vector],
+                              steps: Union[Num, Vector]):
+        # create the criterion
+        criterion = workspace.criterion.Singularities(ik_standard)
+
+        # create the grid calculator object
+        calculator = workspace.GridCalculator(ik_standard,
+                                              archetype,
+                                              criterion,
+                                              lower_bound,
+                                              upper_bound,
+                                              steps)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_2t)
+
+    @pytest.mark.parametrize(
+        ['archetype', 'lower_bound', 'upper_bound', 'steps'],
+        (
+            (
+                workspace.archetype.Translation(dcm),
+                [-1.0, -1.0],
+                [1.0, 1.0],
+                29
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_2t_singularities(self,
+                              robot_2t: Robot,
+                              ik_standard: Kinematics,
+                              archetype: Archetype,
+                              lower_bound: Union[Num, Vector],
+                              upper_bound: Union[Num, Vector],
+                              steps: Union[Num, Vector]):
+        # create the criterion
+        criterion = workspace.criterion.Singularities(ik_standard)
+
+        # create the grid calculator object
+        calculator = workspace.GridCalculator(ik_standard,
+                                              archetype,
+                                              criterion,
+                                              lower_bound,
+                                              upper_bound,
+                                              steps)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_2t)
+
+    @pytest.mark.parametrize(
+        ['archetype', 'lower_bound', 'upper_bound', 'steps'],
+        (
+            (
+                workspace.archetype.Translation(dcm),
+                [-1.0, -1.0],
+                [1.0, 1.0],
+                29
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_2t_wrench_feasible(self,
+                               robot_2t: Robot,
+                               ik_standard: Kinematics,
+                               archetype: Archetype,
+                               lower_bound: Union[Num, Vector],
+                               upper_bound: Union[Num, Vector],
+                               steps: Union[Num, Vector]):
+        # create the criterion
+        criterion = workspace.criterion.WrenchFeasible(
+            force_distribution.ClosedFormImproved(ik_standard, 1, 10), -1)
+
+        # create the grid calculator object
+        calculator = workspace.GridCalculator(ik_standard,
+                                              archetype,
+                                              criterion,
+                                              lower_bound,
+                                              upper_bound,
+                                              steps)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_2t)
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ tests/analysis/workspace/hull/test_hull_3t.py
@@ -0,0 +1,122 @@
+# import sys
+from typing import Union
+
+# import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+from cdpyr.analysis import (
+    force_distribution,
+    workspace
+)
+from cdpyr.analysis.kinematics.algorithm import Algorithm as Kinematics
+from cdpyr.analysis.workspace.archetype.archetype import Archetype
+from cdpyr.kinematics.transformation import Angular
+from cdpyr.robot import Robot
+from cdpyr.typing import (
+    Num,
+    Vector
+)
+
+
+# from mpl_toolkits.mplot3d import Axes3D
+
+
+class HullWorkspace3TTestSuite(object):
+
+    @pytest.mark.parametrize(
+        ['archetype'],
+        (
+            (
+                [workspace.archetype.Translation(dcm)]
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_3t_cable_length(self,
+                             robot_3t: Robot,
+                              ik_standard: Kinematics,
+                              archetype: Archetype):
+        # create the criterion
+        criterion = workspace.criterion.CableLength(ik_standard, [0.5, 1.5])
+
+        # create the hull calculator object
+        calculator = workspace.HullCalculator(ik_standard,
+                                              archetype,
+                                              criterion)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_3t)
+
+    @pytest.mark.parametrize(
+        ['archetype'],
+        (
+            (
+                [workspace.archetype.Translation(dcm)]
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_3t_singularities(self,
+                              robot_3t: Robot,
+                              ik_standard: Kinematics,
+                              archetype: Archetype):
+        # create the criterion
+        criterion = workspace.criterion.Singularities(ik_standard)
+
+        # create the hull calculator object
+        calculator = workspace.HullCalculator(ik_standard,
+                                              archetype,
+                                              criterion)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_3t)
+
+    @pytest.mark.parametrize(
+        ['archetype'],
+        (
+            (
+                [workspace.archetype.Translation(dcm)]
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_3t_singularities(self,
+                              robot_3t: Robot,
+                              ik_standard: Kinematics,
+                              archetype: Archetype):
+        # create the criterion
+        criterion = workspace.criterion.Singularities(ik_standard)
+
+        # create the hull calculator object
+        calculator = workspace.HullCalculator(ik_standard,
+                                              archetype,
+                                              criterion)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_3t)
+
+    @pytest.mark.parametrize(
+        ['archetype'],
+        (
+            (
+                [workspace.archetype.Translation(dcm)]
+            ) for dcm in (np.eye(3), Angular.random().dcm)
+        )
+    )
+    def test_3t_wrench_feasible(self,
+                               robot_3t: Robot,
+                              ik_standard: Kinematics,
+                              archetype: Archetype):
+        # create the criterion
+        criterion = workspace.criterion.WrenchFeasible(
+            force_distribution.ClosedFormImproved(ik_standard, 1, 10), -1)
+
+        # create the hull calculator object
+        calculator = workspace.HullCalculator(ik_standard,
+                                              archetype,
+                                              criterion)
+
+        # evaluate workspace
+        workspace_result = calculator.evaluate(robot_3t)
+
+
+if __name__ == "__main__":
+    pytest.main()
