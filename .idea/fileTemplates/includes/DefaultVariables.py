d248b65407669688af73740cdf1eabc1667347e8
Philipp Tempel <git@philipptempel.me>
2019-12-04 12:46:11 +0100

    WIP: Remove IntelliJ file templates

--- .idea/fileTemplates/includes/DefaultVariables.py
+++
@@ -1,4 +0,0 @@
-#set( $FULLNAME = "Philipp Tempel" )
-#set( $FULLEMAIL = "p.tempel@tudelft.nl" )
-#set( $LICENSE = "EUPL v1.2" )
-#set( $COPYRIGHT = "Copyright 2019, Philipp Tempel" )
--- .idea/fileTemplates/internal/Python Script.py
+++
@@ -1,5 +0,0 @@
-#parse ("DefaultVariables.py")
-__author__ = "${FULLNAME}"
-__email__ = "${FULLEMAIL}"
-__copyright__ = "${COPYRIGHT}"
-__license__ = "${LICENSE}"
