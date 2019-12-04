ca697b64c0a42c2f3cf03df4808dfffd0b7bf9c2
Philipp Tempel <git@philipptempel.me>
2019-12-04 20:17:20 +0100

    Implement geometry plot test suite for all engines and primitives

--- tests/visualization/test_plot_geometry.py
+++ tests/visualization/test_plot_geometry.py
@@ -3,10 +3,8 @@ import itertools
 import pytest
 
 from cdpyr import visualization
-from cdpyr.geometry import (
-    Cuboid,
-    Geometry
-)
+from cdpyr.geometry import Cuboid, Cylinder, Ellipsoid, Tube
+from cdpyr.geometry.primitive import Primitive
 
 __author__ = "Philipp Tempel"
 __email__ = "p.tempel@tudelft.nl"
@@ -16,21 +14,23 @@ class PlotRobotTestSuite(object):
 
     @pytest.mark.parametrize(
         ('engine', 'geometry'),
-        itertools.product((visualization.engine.plotly.Linear(),
-                           visualization.engine.plotly.Planar(),
-                           visualization.engine.plotly.Spatial()),
+        itertools.product((visualization.engine.plotly.Linear,
+                           visualization.engine.plotly.Planar,
+                           visualization.engine.plotly.Spatial,),
                           (
-                              # Cylinder(1.00, 2.00),
                               Cuboid(1.00, 2.00, 3.0),
-                              # EllipticCylinder(1.00, 2.00, 3.00),
-                              # Tube(1.00, 2.00, 3.00),
-                              # Sphere(1.00))
+                              Cylinder(1.00, 2.00),
+                              Cylinder([1.00, 2.00], 3.00),
+                              Ellipsoid(1.00),
+                              Ellipsoid([1.00, 2.00, 3.00]),
+                              Tube(1.00, 2.00, 3.00),
+                              Tube([1.00, 2.00], 3.00, 4.00),
+                              Tube([1.00, 2.00], [3.00, 4.00], 5.00),
                           )
                           )
     )
-    def test_render(self, geometry: Geometry,
-                    engine: visualization.engine.Engine):
-        wizard = visualization.Visualizer(engine)
+    def test_render(self, engine: visualization.engine.Engine, geometry: Primitive):
+        wizard = visualization.Visualizer(engine())
         wizard.render(geometry)
         wizard.draw()
         wizard.show()
