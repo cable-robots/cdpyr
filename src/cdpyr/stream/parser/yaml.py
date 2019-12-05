7360af0d80fc547e0212681dcedd4ee09f31cdd9
Philipp Tempel <git@philipptempel.me>
2019-12-05 09:43:02 +0100

    Add support to export/import from/to `OrderedDicts` in YAML

--- src/cdpyr/stream/parser/yaml.py
+++ src/cdpyr/stream/parser/yaml.py
@@ -6,6 +6,20 @@ import yaml
 from cdpyr.stream.parser import parser as _parser
 
 
+def ordered_dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+
+
+def ordered_dict_constructor(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+
+
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+yaml.Dumper.add_representer(OrderedDict, ordered_dict_representer)
+yaml.Loader.add_constructor(_mapping_tag, ordered_dict_constructor)
+
+
 class Yaml(_parser.Parser):
 
     def dumps(self, d: Union[OrderedDict, dict], *args, **kwargs) -> AnyStr:
