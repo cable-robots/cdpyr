ce7041d9f317eb1d0936990c38fdc9e39ed8f637
Philipp Tempel <git@philipptempel.me>
2019-12-05 09:42:32 +0100

    Add missing schema class `CdpyrSchema`

---
+++ src/cdpyr/schema/cdpyr_schema.py
@@ -0,0 +1,31 @@
+from marshmallow import Schema, fields
+
+__author__ = "Philipp Tempel"
+__email__ = "p.tempel@tudelft.nl"
+
+
+class CdpyrSchema(Schema):
+
+    class Meta:
+        # ensures the order of fields is kept when exporting
+        ordered = True
+
+    version = fields.String(
+            required=False,
+            attribute='VERSION',
+    )
+    # version_out = fields.Raw(
+    #         required=False,
+    #         dump_only=True,
+    #         attribute='version'
+    # )
+    # version_in = fields.Raw(
+    #         required=False,
+    #         load_only=True,
+    #         data_key='version'
+    # )
+
+
+__all__ = [
+        'CdpyrSchema',
+]
