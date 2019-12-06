d28be84105d34a433d23b782bf50be7d89e9d106
Philipp Tempel <git@philipptempel.me>
2019-12-06 14:07:08 +0100

    Update geometry test cases to support `center` and `centroid `validation

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -15,51 +15,107 @@ class GeometryTestSuite(object):
     @pytest.mark.parametrize(
             ('geometry', 'expected'),
             (
-                    (  # 0
+                    (
                             geometry.Cuboid(1, 2, 3),
                             {'surface': 22,
-                             'volume':  6}),
-                    (  # 1
+                             'volume':  6,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Cuboid(1, 2, 3, [-1, -1, -1]),
+                            {'surface': 22,
+                             'volume':  6,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Cylinder(1, 2),
                             {'surface': 18.849555921539,
-                             'volume':  6.2831853071796}),
-                    (  # 2
+                             'volume':  6.2831853071796,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Cylinder(1, 2, [-1, -1, -1]),
+                            {'surface': 18.849555921539,
+                             'volume':  6.2831853071796,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Cylinder([1, 2], 3),
                             {'surface': 41.631715276002,
-                             'volume':  18.849555921539}),
-                    (  # 3
+                             'volume':  18.849555921539,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Cylinder([1, 2], 3, [-1, -1, -1]),
+                            {'surface': 41.631715276002,
+                             'volume':  18.849555921539,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Ellipsoid(1),
                             {'surface': 12.56637061435917295385,
-                             'volume':  4.188790204786390984617}),
-                    (  # 4
+                             'volume':  4.188790204786390984617,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Ellipsoid(1, [-1, -1, -1]),
+                            {'surface': 12.56637061435917295385,
+                             'volume':  4.188790204786390984617,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Ellipsoid([1, 2, 3]),
                             {'surface': 48.8821463025820596957,
-                             'volume':  25.1327412287183459077}),
-                    (  # 5
+                             'volume':  25.1327412287183459077,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Ellipsoid([1, 2, 3], [-1, -1, -1]),
+                            {'surface': 48.8821463025820596957,
+                             'volume':  25.1327412287183459077,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Tube(1, 2, 3),
-                            {'surface': 62.831853071796 -
-                                        25.132741228718 + 2 *
-                                        18.849555921539,
-                             'volume':  37.699111843078 -
-                                        9.4247779607694}),
-                    (  # 6
+                            {'surface': 75.398223686156,
+                             'volume':  28.2743338823086,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Tube(1, 2, 3, [-1, -1, -1]),
+                            {'surface': 75.398223686156,
+                             'volume':  28.2743338823086,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Tube([1, 2], 3, 4),
-                            {'surface': 131.94689145077 -
-                                        51.32016349655 + 2 *
-                                        38.753792882191,
-                             'volume':  113.09733552923 -
-                                        25.132741228718}),
-                    (  # 7
+                            {'surface': 158.13431371860202,
+                             'volume':  87.96459430051199,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Tube([1, 2], 3, 4, [-1, -1, -1]),
+                            {'surface': 158.13431371860202,
+                             'volume':  87.96459430051199,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
+                    (
                             geometry.Tube([1, 2], [3, 4], 5),
-                            {'surface': 185.9156844897 -
-                                        61.008611717098 + 2 *
-                                        48.442241102738,
-                             'volume':  188.49555921539 -
-                                        31.415926535898}),
+                            {'surface': 221.791554978078,
+                             'volume':  157.079632679492,
+                             'center': [0, 0, 0],
+                             'centroid': [0, 0, 0]}),
+                    (
+                            geometry.Tube([1, 2], [3, 4], 5, [-1, -1, -1]),
+                            {'surface': 221.791554978078,
+                             'volume':  157.079632679492,
+                             'center': [-1, -1, -1],
+                             'centroid': [-1, -1, -1]}),
             )
     )
     def test_surfaces(self, geometry: Primitive, expected: Mapping):
-        assert geometry.centroid == pytest.approx(_np.zeros((3,)))
+        assert geometry.center == pytest.approx(expected['center'])
+        assert geometry.centroid == pytest.approx(expected['centroid'])
         assert geometry.surface == pytest.approx(expected['surface'], rel=1e-2)
         assert geometry.volume == pytest.approx(expected['volume'], rel=1e-2)
 
