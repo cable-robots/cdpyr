49e2b377ea5eb051c5a2c1ff40e1bf52e52616d7
Philipp Tempel <git@philipptempel.me>
2020-01-09 11:36:59 +0100

    Fix incorrect field of `faces` in `Polyhedron` schema

--- src/cdpyr/schema/geometry/polyhedron.py
+++ src/cdpyr/schema/geometry/polyhedron.py
@@ -9,19 +9,18 @@ __email__ = "p.tempel@tudelft.nl"
 
 class PolyhedronSchema(_geometry.PrimitiveSchema):
     vertices = fields.List(
-            fields.Tuple(
-                    (fields.Float(required=True),
-                     fields.Float(required=True),
-                     fields.Float(required=True))
-            ),
+            fields.Tuple((fields.Float(required=True),
+                          fields.Float(required=True),
+                          fields.Float(required=True))
+                         ),
             required=True,
     )
+
     faces = fields.List(
-            fields.Tuple(
-                    (fields.Float(required=True),
-                     fields.Float(required=True),
-                     fields.Float(required=True))
-            ),
+            fields.Tuple((fields.Integer(required=True),
+                          fields.Integer(required=True),
+                          fields.Integer(required=True))
+                         ),
             required=True,
     )
 
