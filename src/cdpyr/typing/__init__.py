989080be431bd2bd93e9f35a23a2649e38049336
Philipp Tempel <git@philipptempel.me>
2020-01-13 14:51:59 +0100

    Add type hints for a polyhedron `Face` and `Vertex` as well as `Faces` and `Vertices`

--- src/cdpyr/typing/__init__.py
+++ src/cdpyr/typing/__init__.py
@@ -1,16 +1,24 @@
-from typing import Sequence, Union
+from typing import Sequence, Tuple, Union
 
-import numpy as np_
+import numpy as _np
 
 __author__ = "Philipp Tempel"
 __email__ = "p.tempel@tudelft.nl"
 
 Num = Union[int, float]
-Vector = Union[Sequence[Num], np_.ndarray]
-Matrix = Union[Sequence[Sequence[Num]], np_.ndarray]
+Vector = Union[Sequence[Num], _np.ndarray]
+Matrix = Union[Sequence[Sequence[Num]], _np.ndarray]
+Vertex = Union[Tuple[float, float, float], _np.ndarray]
+Vertices = Tuple[Vertex, ...]
+Face = Union[Tuple[int, int, int], _np.ndarray]
+Faces = Tuple[Face, ...]
 
 __all__ = [
+        'Face',
+        'Faces',
+        'Matrix',
         'Num',
         'Vector',
-        'Matrix',
+        'Vertices',
+        'Vertex',
 ]
