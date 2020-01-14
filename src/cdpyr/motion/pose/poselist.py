657d73d376703da89ea887490e7227035a74e576
Philipp Tempel <git@philipptempel.me>
2020-01-14 15:05:21 +0100

    Sort code alphabetically

--- src/cdpyr/motion/pose/poselist.py
+++ src/cdpyr/motion/pose/poselist.py
@@ -11,36 +11,36 @@ __email__ = "p.tempel@tudelft.nl"
 class PoseList(UserList, BaseObject):
 
     @property
-    def time(self):
-        return (pose.time for pose in self.data)
-
-    @property
-    def position(self):
-        return (pose.position for pose in self.data)
-
-    @property
-    def velocity(self):
-        return (pose.velocity for pose in self.data)
-
-    @property
     def acceleration(self):
         return (pose.acceleration for pose in self.data)
-
-    @property
-    def linear(self):
-        return (pose.linear for pose in self.data)
 
     @property
     def angular(self):
         return (pose.angular for pose in self.data)
 
     @property
-    def transformation(self):
-        return (pose.transformation for pose in self.data)
+    def linear(self):
+        return (pose.linear for pose in self.data)
+
+    @property
+    def position(self):
+        return (pose.position for pose in self.data)
 
     @property
     def state(self):
         return (pose.state for pose in self.data)
+
+    @property
+    def time(self):
+        return (pose.time for pose in self.data)
+
+    @property
+    def transformation(self):
+        return (pose.transformation for pose in self.data)
+
+    @property
+    def velocity(self):
+        return (pose.velocity for pose in self.data)
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
