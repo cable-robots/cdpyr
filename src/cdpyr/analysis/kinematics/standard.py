b84381fbd5081c34e0784b52903de0e38509a13e
Philipp Tempel <git@philipptempel.me>
2020-03-16 08:58:16 +0100

    Update standard forward kinematics to use Tait-Bryan angles instead of quaternions

--- src/cdpyr/analysis/kinematics/standard.py
+++ src/cdpyr/analysis/kinematics/standard.py
@@ -38,27 +38,21 @@ class Standard(_algorithm.Algorithm):
                 [platform.anchors[anchor_index].linear.position for anchor_index
                  in kcs.platform_anchor]).T
 
-        # scaling of all parameters to increase sensitivity of the optimizer
-        scaling = _np.asarray([0.1, 0.1, 0.1, 10.0, 10.0, 10.0, 10.0]) * 10
-
         # initial directions needed for later returned result
         last_direction = []
 
         # goal function for the estimator
-        def goal_function(x):
-            # remove scaling
-            x = x / scaling
-
-            # estimate position
+        def goal_function(x, ai, bi, *args, **kwargs):
+            # position estimate
             pos = x[0:3]
-            # estimated rotation matrix from quaternions
-            dcm = _angular.Angular(quaternion=x[3:7]).dcm
+            # euler angle estimate to rotation matrix
+            dcm = _angular.Angular(sequence='xyz', euler=x[3:6]).dcm
 
             # solve the inverse kinematics vector loop
             directions = self._vector_loop(pos,
                                            dcm,
-                                           frame_anchors,
-                                           platform_anchors)
+                                           ai,
+                                           bi)
 
             # strip additional spatial dimensions
             directions = directions[0:platform.motion_pattern.dof_translation,
@@ -69,92 +63,69 @@ class Standard(_algorithm.Algorithm):
             # cable lengths
             estimated_lengths = _np.linalg.norm(directions, axis=0)
 
-            # return error as || l - l_in || ^ 2
-            return _np.linalg.norm(estimated_lengths - lengths) ** 2
+            # return error as (l^2 - l(x)^2)
+            return lengths ** 2 - estimated_lengths ** 2
 
         # initial pose estimate given by user?
-        initial_estimate = kwargs.get('x0', None)
+        initial_estimate = kwargs.pop('x0', None)
         # no initial pose estimate given, so calculate one based on Schmidt.2016
         if initial_estimate is None:
-            # initial_estimate = self._pose_estimate(robot, lengths)
-            initial_estimate = _np.asarray([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+            initial_estimate = self._pose_estimate(robot, lengths)
 
-        # introduce scaling
-        initial_estimate *= scaling
+        # scaling of all parameters to increase sensitivity of the optimizer
+        x_scale = kwargs.pop('x_scale', None)
+        if x_scale is None:
+            x_scale = _np.asarray([0.01, 0.01, 0.01, 100.0, 100.0, 100.0])
 
         # bounds of estimation
-        bounds = optimize.Bounds([-_np.inf, -_np.inf, -_np.inf, -1, -1, -1, -1],
-                                 [_np.inf, _np.inf, _np.inf, 1, 1, 1, 1])
+        bounds = ([-_np.inf, -_np.inf, -_np.inf,  # [x, y, z]
+                   -0.5 * _np.pi, -0.5 * _np.pi, -0.5 * _np.pi],  # [r, p, y]
+                  [_np.inf, _np.inf, _np.inf,  # [x, y, z]
+                   0.5 * _np.pi, 0.5 * _np.pi, 0.5 * _np.pi])  # [r, p, y]
 
         # minimization method to use
-        method: str = kwargs.get('method', 'COBYLA')
+        method: str = kwargs.pop('method', 'lm')
 
-        # tolerances on step size, residual size, and constraint residual size
-        xtol, ftol, gtol = 1e-14, 1e-12, 1e-14
+        # tolerances for solver
+        ftol = _np.sqrt(_np.finfo(float).eps) / 1e5
+        xtol = _np.sqrt(_np.finfo(float).eps) / 1e5
+        gtol = 0.0
 
-        # default options of all supported minimization methods
-        method_option_defaults = {
-                'Nelder-Mead': {
-                        'xtol': xtol,
-                        'ftol': ftol,
-                },
-                'Powell':      {
-                        'xtol': xtol,
-                        'ftol': ftol,
-                },
-                'CG':          {
-                        'gtol': gtol,
-                },
-                'BFGS':        {
-                        'gtol': gtol,
-                },
-                'Newton-CG':   {
-                        'xtol': xtol,
-                },
-                'L-BFGS-B':    {
-                        'ftol':  ftol,
-                        'gtol':  gtol,
-                        'factr': 1e4,
-                },
-                'TNC':         {
-                        'xtol': xtol,
-                        'ftol': ftol,
-                        'gtol': gtol,
-                },
-                'COBYLA':      {
-                        'tol': ftol,
-                },
-                'SLSQP':       {
-                        'ftol': ftol,
-                },
-                'dogleg':      {
-                        'gtol': gtol,
-                },
-                'trust-ncg':   {
-                        'gtol': gtol,
+        # default keyword arguments to `least_squares`
+        defaults = {
+                'method':  method,
+                'ftol':    ftol,
+                'xtol':    xtol,
+                'gtol':    gtol,
+                'x_scale': x_scale,
+                'jac':     '3-point',
+                'args':    (
+                        frame_anchors,
+                        platform_anchors,
+                ),
+                'kwargs':  {
+                        'ai': frame_anchors,
+                        'bi': platform_anchors
                 },
         }
+        # only add `bounds` if the solver is not `lm` (levenberg-marquardt)
+        if method != 'lm':
+            defaults['bounds'] = bounds
+        # update with user-supplied kwargs
+        defaults.update(kwargs)
 
-        method_options = {'maxiter': 7 * 314, 'disp': False}
-        try:
-            method_options += method_option_defaults[method.lower()]
-        except (KeyError, TypeError):
-            pass
-
-        # estimate the pose
+        # estimate pose
         result: optimize.OptimizeResult
-        result = optimize.minimize(goal_function,
-                                   initial_estimate,
-                                   # bounds=bounds,
-                                   options=method_options)
+        result = optimize.least_squares(goal_function,
+                                        initial_estimate,
+                                        **kwargs)
 
         # check for convergence
         try:
             if result.success is not True:
                 raise ArithmeticError(
                         f'Optimizer did not exit successfully. Last message '
-                        f'was '
-                        f'{result.message}')
+                        f'was {result.message}')
         except ArithmeticError as ArithmeticE:
             raise ValueError(
                     'Unable to solve the standard forward kinematics for the '
@@ -163,11 +134,12 @@ class Standard(_algorithm.Algorithm):
                     'arguments to the underlying optimization method.') from \
                 ArithmeticE
         else:
-            final = result.x / scaling
+            final = result.x
+            final[3:6] = _np.fmod(final[3:6], 2 * _np.pi)
 
             pose = _pose.Pose(
                     final[0:3],
-                    angular=_angular.Angular(quaternion=final[3:7])
+                    angular=_angular.Angular(sequence='xyz', euler=final[3:6])
             )
 
             return _algorithm.Result(self, robot, pose, lengths=lengths,
@@ -211,7 +183,7 @@ class Standard(_algorithm.Algorithm):
         lengths = _np.linalg.norm(directions, axis=0)
 
         # unit directions
-        directions = directions / lengths
+        directions /= lengths
 
         # to avoid divisions by zero yielding `NaN`, we will set all unit
         # vectors to zero where the cable length is zero. technically,
@@ -238,7 +210,7 @@ class Standard(_algorithm.Algorithm):
         lengths = _np.asarray(lengths)
 
         # initialize estimated initial position
-        estimate = _np.asarray([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+        estimate = _np.asarray([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
         # for now, this is the inner-loop code for the first platform
         index_platform = 0
