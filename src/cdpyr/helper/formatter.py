27c6496fac7e904f508c6c241b1a93a146993809
Philipp Tempel <git@philipptempel.me>
2020-06-09 15:41:28 +0200

    Add helper `formatter.sane_string`

--- src/cdpyr/helper/formatter.py
+++ src/cdpyr/helper/formatter.py
@@ -4,15 +4,14 @@ __author__ = "Philipp Tempel"
 __email__ = "p.tempel@tudelft.nl"
 __all__ = [
         'filesize',
+        'sane_string',
         'time',
         'units',
 ]
 
-from typing import AnyStr, Iterable, Optional
+from typing import AnyStr, Iterable, Optional, Union
 
-import hurry.filesize
-
-filesize = hurry.filesize
+import hurry.filesize as filesize
 
 units = {
         "nsec": 1e-9,
@@ -22,7 +21,7 @@ units = {
 }
 
 
-def time(dt,
+def time(dt: Union[float, Iterable[float]],
          time_unit: Optional[AnyStr] = None,
          precision: Optional[int] = None):
     if isinstance(dt, Iterable):
@@ -42,3 +41,26 @@ def time(dt,
     return "%.*g %s" % (precision if precision is not None else 3,
                         dt / scale,
                         unit)
+
+
+def sane_string(s: str, keep: Iterable = None):
+    """
+    Create a sane string i.e., remove all non-alphanumeric characters
+
+    Parameters
+    ----------
+    s : str
+        Unsafe string to parse
+    keep : Optional[Iterable]
+        A sequence of additional characters to keep besides alphanumeric and the
+        default (' ', '.', '_')
+
+    Returns
+    -------
+    s : str
+        A sane(r?) string
+    """
+    if keep is None:
+        keep = (' ', '.', '_')
+
+    return "".join(c for c in s if c.isalnum() or c in keep).rstrip()
