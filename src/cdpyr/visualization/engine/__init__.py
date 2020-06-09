bdeb5b2329c6e1c9871afc2eee38fb5a034aedc3
Philipp Tempel <git@philipptempel.me>
2020-06-09 11:21:21 +0200

    Remove automatic import of plotting engines
    If a user does not have plotly or mayavi, the whole import of
    `cdpyr.visualization` would fail. Now, the user has to explicitly import
    the desired visualization engine e.g., `from cdpyr.visualization.engine
    import plotly`

--- src/cdpyr/visualization/engine/__init__.py
+++ src/cdpyr/visualization/engine/__init__.py
@@ -1,10 +1,4 @@
 __author__ = "Philipp Tempel"
 __email__ = "p.tempel@tudelft.nl"
 __all__ = [
-        'mayavi',
-        'plotly',
-        'Engine',
 ]
-
-from cdpyr.visualization.engine import mayavi, plotly
-from cdpyr.visualization.engine.engine import Engine
